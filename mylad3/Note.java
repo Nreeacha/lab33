fb6130cfd6b5f4f6e89ff9845890562d41d2e714
Nreeacha <nreeacha.poo67@psru.ac.th>
2025-07-18 17:11:35 +0700

    Add files via upload

---
+++ mylad3/Note.java
@@ -0,0 +1,14 @@
+package com.example.mylad3;
+
+public class Note {
+    String title;
+    String context;
+    int createdDate;
+
+    void getSmmary(){
+        System.out.println(title);
+        System.out.println(context);
+        System.out.println(createdDate);
+
+    }
+}
---
+++ mylad3/User.java
@@ -0,0 +1,9 @@
+package com.example.mylad3;
+
+public class User {
+    String name;
+    int id;
+    void take_note(){
+
+    }
+}
